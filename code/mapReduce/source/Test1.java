defec8ea5ebab617ad0b31eb541f5ba4a4f48a73
Bhavik Mistry <mbhavik91@users.noreply.github.com>
2016-07-25 18:42:56 -0500

    Update Test1.java

--- code/mapReduce/source/Test1.java
+++ code/mapReduce/source/Test1.java
@@ -9,7 +9,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 //import org.apache.hadoop.mapred.*;
 // import org.apache.hadoop.mapred.FileOutputFormat;
-// org.apache.hadoop.mapreduce.Job;
+org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
